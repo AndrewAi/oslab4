3e52d14c01b9ea50a5d7dc903c796c7c32bb1edb
AndrewAi <andrewiriwin@gmail.com>
2016-10-13 17:47:05 +0100

    added fCFS method to main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,62 +8,33 @@ public class Main {
     public static void main(String[] args) {
 
 
-        int timeQuantum = 3;
+
         int count = 0;
+        boolean firstLoop = true;
+
 
         ArrayList<Process> processList = new ArrayList<Process>();
 
         processList.add(new Process(1,12,0));
-        processList.add(new Process(2,12,0));
+        processList.add(new Process(2,20,0));
         processList.add(new Process(3,12,0));
-        processList.add(new Process(4,12,0));
-        processList.add(new Process(5,12,0));
+        //processList.add(new Process(4,24,0));
+        //processList.add(new Process(5,27,0));
 
 
 
 
-        while (processList.isEmpty() == false) {
 
 
 
-            for (Process p : processList) {
-
-                //minus the timeQuantum from the processTime
-                p.setProcessTime(timeQuantum);
-
-
-                if (p.getProcessTime() <= timeQuantum) {
-                    processList.remove(p);
-                }
 
 
 
-                int waitTime = (p.getprocessId()-1) * timeQuantum;
 
-                System.out.println("list: " + processList.size());
-                System.out.println("p" + p.getprocessId() + ": " + p.getProcessTime());
-                System.out.println("p" + p.getprocessId() + " wt: " + waitTime);
-
-
-
-                p.setProcessWaitTime(waitTime);
-
-                //find out how to add to the list i.e update the wait time of a list item
-                //processList.indexOf(p.getprocessId());
-
-
-                System.out.println("Process " + p.getprocessId() + " Wait time: " + p.getProcessWaitTime());
-                System.out.println();
-
-                /*if (count == processList.size()){
-                    count = 0;
-                }
-                */
-            }
-        }
 
 
         //sjfSorter(processList);
+        fCFS(processList);
 
 
     }
@@ -79,6 +50,90 @@ public class Main {
         }
 
 
+    }
+
+
+
+
+
+    private static void fCFS(ArrayList<Process> processList){
+
+
+        int totalTime = 0;
+
+        for (int i = 0; i < processList.size(); i++){
+
+            if (processList.get(i).getprocessId() == 1) {
+                processList.get(i).setProcessWaitTime(0);
+            }
+            else {
+
+                totalTime += processList.get(i).getProcessTime();
+                processList.get(i).setProcessWaitTime(totalTime);
+            }
+
+
+
+            System.out.println("p" + processList.get(i).getprocessId() + ": " + processList.get(i).getProcessWaitTime());
+        }
+
+
+    }
+
+
+
+
+
+
+    public void roundRobin(ArrayList<Process> processList) {
+
+        int waitTime = 0;
+        int timeQuantum = 3;
+
+        while (processList.isEmpty() == false) {
+
+
+            for (int i = 0; i < processList.size(); i++) {
+
+                //minus the timeQuantum from the processTime
+                processList.get(i).setProcessTime(timeQuantum);
+
+
+                //int waitTime = (p.getprocessId()-1) * timeQuantum;
+                //System.out.println(firstLoop);
+
+
+                waitTime = (processList.get(i).getprocessId() - 1) * timeQuantum;
+
+
+                int totaWaitTime = +waitTime;
+                int averageWaitTime = totaWaitTime / processList.size();
+                //System.out.println(firstLoop);
+
+
+                System.out.println("list: " + processList.size());
+                System.out.println("p" + processList.get(i).getprocessId() + ": " + processList.get(i).getProcessTime());
+                System.out.println("p" + processList.get(i).getprocessId() + " wt: " + waitTime);
+
+
+                processList.get(i).setProcessWaitTime(waitTime);
+
+                //find out how to add to the list i.e update the wait time of a list item
+                //processList.indexOf(p.getprocessId());
+
+
+                System.out.println("P" + processList.get(i).getprocessId() + " Wait time: " + processList.get(i).getProcessWaitTime());
+                System.out.println();
+
+
+
+                if (processList.get(i).getProcessTime() < timeQuantum) {
+                    processList.remove(i);
+                    //continue;
+                }
+
+            }
+        }
     }
 
 }
